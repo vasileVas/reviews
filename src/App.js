91b7aafc1b7df312427f2791e78f3ba31384c742
Radu Pacurar <radu.pacurar@softvision.ro>
2018-07-17 16:54:19 +0300

    added react router, mobx, routes for app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Reviews from './components/Reviews';
+import AddReview from './components/AddReview';
+
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+    border: 1px solid blue;
+    margin: auto
+    width: 500px
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <AppWrapper>
+                    <Switch>
+                        <Route exact path="/" component={Reviews} />
+                        <Route path="/reviews" component={Reviews} />
+                        <Route path="/add-review" component={AddReview} />
+                    </Switch>
+                </AppWrapper>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Reviews.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    font-size: 18px;
+`;
+
+export default () => <Div>Reviews</Div>;
